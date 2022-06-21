6a75c818316609c49991cc5254ac36ccdb7b9e57
David Olamilekan Raj (Tekie) <98900519+Mybebe1@users.noreply.github.com>
2022-06-21 20:42:02 +0100

    first javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("task").innerHTML = "First Name: David";
+document.getElementById("last").innerHTML = "Last Name: Olamilekan";
+document.getElementById("height").innerHTML = "Height: 6ft";
+document.getElementById("country").innerHTML = "Country: Nigeria";
